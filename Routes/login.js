c278f13689fbd4e979fd4382fea629903702cc9a
LLUIS CANTON CALLAU <lluis.cantoncallau@altran.com>
2020-04-26 11:20:02 +0200

    googleLogin

--- Routes/login.js
+++ Routes/login.js
@@ -4,6 +4,9 @@ var bodyParser = require('body-parser');
 var bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
 var SEED = require('../config/config').SEED;
+const { OAuth2Client } = require('google-auth-library');
+var CLIENT_ID = require('../config/config').CLIENT_ID;
+const client = new OAuth2Client(CLIENT_ID);
 var app = express();
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.post('/', (req, res)=> {
+app.post('/', (req, res) => {
     var body = req.body;
 
     Usuario.findOne({ email: body.email }, (err, userDb) => {
@@ -31,7 +34,7 @@ app.post('/', (req, res)=> {
             });
         }
 
-        if(!bcrypt.compareSync(body.password, userDb.password)){
+        if (!bcrypt.compareSync(body.password, userDb.password)) {
             return res.status(400).json({
                 ok: false,
                 mensaje: 'Credenciales incorrectas',
@@ -41,15 +44,103 @@ app.post('/', (req, res)=> {
 
         //Crear Token
         userDb.password = '';
-        var token = jwt.sign({ usuario: userDb}, SEED, { expiresIn: 14400 }); 
+        var token = jwt.sign({ usuario: userDb }, SEED, { expiresIn: 14400 });
         res.status(200).json({
             ok: true,
-           usuario: userDb,
-           token: token,
-           id: userDb.id
+            usuario: userDb,
+            token: token,
+            id: userDb.id
         });
     });
-   
+
 });
+
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID,
+    });
+
+    const payload = ticket.getPayload();   
+    return {
+        nombre: payload.name,
+        google: true,
+        email: payload.email,
+        img: payload.picture
+    }
+}
+
+app.post('/google', async (req, res) => {
+    var token = res.body.token;
+    var googleUser = await verify(token)
+        .catch(e=> {
+            return res.status(403).json({
+                ok: false,
+                mensaje: 'Token no valido'
+            });
+        });
+
+    Usuario.findOne({email: googleUser.email}, (err, usuarioDb) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error en mongo DB',
+                errors: err
+            });
+        }
+        if (usuarioDb) {
+            if(!usuarioDb.google) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Debe de usar su authenticacion de la aplicacion',
+                    errors: err
+                });
+            }
+
+            userDb.password = '';
+            var token = jwt.sign({ usuario: usuarioDb }, SEED, { expiresIn: 14400 });
+            res.status(200).json({
+                ok: true,
+                usuario: usuarioDb,
+                token: token,
+                id: usuarioDb.id
+            });
+
+        }
+
+        var usuario = new Usuario({
+            nombre: googleUser.nombre,
+            email: googleUser.email,          
+            img: googleUser.img,
+            password: ':)'           
+        });
+    
+        usuario.save((err, usuarioDb) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error en mongo DB',
+                    errors: err
+                });
+            }
+            var token = jwt.sign({ usuario: usuarioDb }, SEED, { expiresIn: 14400 });
+            res.status(201).json({
+                ok: true,
+                usuario: usuarioDb,
+                token: token,
+                id: usuarioDb._id
+            });
+        });
+
+    });
+
+    return res.status(200).json({
+        ok: true,
+        mensaje: 'Ok',
+        googleUser: googleUser
+    });
+});
+
 
 module.exports = app;
--- config/config.js
+++ config/config.js
@@ -1,1 +1,3 @@
 module.exports.SEED = '@23213fdsjfyuew535gdhfts632%v565342';
+
+module.exports.CLIENT_ID = '3157745092-g0lt2jp0i5cp9ku8813jc3sg5ind6hh4.apps.googleusercontent.com';
